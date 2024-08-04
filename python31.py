7c5eca629b5787d68c3ad8a62cf98bb60234865a
Leonardo Barros <leonardobarros9931@gmail.com>
2024-08-04 18:58:01 -0300

    adicionados mais comandos

--- python31.py
+++ python31.py
@@ -7,13 +7,22 @@ password = getpass.getpass() #solicita ao usuário para entrar com senha
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"login: ")
+tn.read_until(b"Username: ")
 tn.write(user.encode('ascii') + b"\n")
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-tn.write(b"ls\n")
+tn.write(b"enable\n")
+tn.write(b"cisco\n") #apenas por questão de teste foi implantada esta linha, futuras atualizações removarão senhas e usuários do script
+tn.write(b"conf t\n")
+tn.write(b"int loop 0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+tn.write(b"int loop 1\n")
+tn.write(b"ip address 2.2.2.2 255.255.255.255\n")
+tn.write(b"router ospf 1\n")
+tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write(b"end\n")
 tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
